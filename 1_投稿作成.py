07892ef723546bffb432cd61751537e7cfaa0501
marumaru1019 <ryu.ryusei401@gmail.com>
2024-04-23 23:49:20 +0900

    タイトルをレコメンドする処理を追加

--- 1_投稿作成.py
+++ 1_投稿作成.py
@@ -13,7 +13,7 @@ st.title('SAKIYOMI 投稿作成AI')
 st.sidebar.title('メニュー')
 
 # タブセット1: "Input / Generated Script" を含むタブ
-tab1, tab2 = st.tabs(["プロット生成", "データ登録"])
+tab1, tab2, tab3 = st.tabs(["プロット生成", "データ登録", "タイトル候補"])
 
 with tab1:
     col1, col2 = st.columns(2)
@@ -23,6 +23,7 @@ with tab1:
         url = st.text_input("参考URL")
         selected_llm = st.radio("LLMの選択", ("GPT-4", "Claude3"))
         submit_button = st.button('送信')
+        title_submit_button = st.button('タイトル生成')
 
     if submit_button:
         if 'last_url' not in st.session_state or (st.session_state['last_url'] != url or url == ""):
@@ -56,12 +57,24 @@ with tab1:
             else:
                 st.session_state['response_text'] = "エラー: プロットを生成できませんでした。"
 
+        if title_submit_button:
+            # Pineconeインデックスの初期化
+            index = sh.initialize_pinecone()
+
+            # クエリの実行
+            query_results = sh.perform_similarity_search(index, "*", "ns3", top_k=5)
+            titles = sh.get_search_results_titles(query_results)
+            original_titles = sh.generate_new_titles("*", titles)
+            display_titles = [f"- {title}" for title in original_titles.split('\n') if title.strip()]  # 空行を除外
+            if original_titles:
+                response_text = [f"- {title}" for title in original_titles.split('\n') if title.strip()]  # 空行を除外
+                st.session_state['response_text'] = "\n".join(response_text)
+            else:
+                st.session_state['response_text'] = "エラー: プロットを生成できませんでした。"
+
         # セッション状態からresponse_textを取得、存在しない場合はデフォルトのメッセージを表示
         displayed_value = st.session_state.get('response_text', "生成結果 : プロットが表示されます")
         st.text_area("生成結果", value=displayed_value, height=400)
-  
-
-
 
 
 # タブ2: パラメーター設定
@@ -70,7 +83,7 @@ with tab2:
     # 2カラムを作成
     col1, col2, col3, col4 = st.columns(4)
 
-    with col1:  
+    with col1:
         index = sh.initialize_pinecone()
         st.subheader("URLの登録")
 
@@ -118,10 +131,10 @@ with tab2:
         if register_button2 and pdf_file1 is not None:
             # PDFファイルからテキストを抽出
             pdf_text = sh.extract_text_from_pdf(pdf_file1)
-            
+
             # テキストをチャンクに分割
             chunks = sh.split_text(pdf_text)
-            
+
             # チャンクの埋め込みを生成
             embeddings = sh.make_chunks_embeddings(chunks)
 
@@ -144,7 +157,7 @@ with tab2:
         st.subheader("競合データの登録")
 
         # PDFファイルアップロード
-        pdf_file2 = st.file_uploader("PDFファイルをアップロード", type=["pdf"], key="pdf_file2")   
+        pdf_file2 = st.file_uploader("PDFファイルをアップロード", type=["pdf"], key="pdf_file2")
 
         # 登録ボタン
         register_button3 = st.button("PDF登録", key="register_button3")
@@ -152,10 +165,10 @@ with tab2:
         if register_button3 and pdf_file2 is not None:
             # PDFファイルからテキストを抽出
             pdf_text = sh.extract_text_from_pdf(pdf_file2)
-            
+
             # テキストをチャンクに分割
             chunks = sh.split_text(pdf_text)
-            
+
             # チャンクの埋め込みを生成
             embeddings = sh.make_chunks_embeddings(chunks)
 
@@ -185,10 +198,10 @@ with tab2:
         if register_button4 and pdf_file3 is not None:
             # PDFファイルからテキストを抽出
             pdf_text = sh.extract_text_from_pdf(pdf_file3)
-            
+
             # テキストをチャンクに分割
             chunks = sh.split_text(pdf_text)
-            
+
             # チャンクの埋め込みを生成
             embeddings = sh.make_chunks_embeddings(chunks)
 
@@ -204,3 +217,44 @@ with tab2:
             # 全データを削除する関数を呼び出し
             sh.delete_all_data_in_namespace(index, "ns5")
             st.success("全データが削除されました！")
+
+# 検索タブ
+with tab3:
+    st.header("検索条件を入力")
+    col1, col2 = st.columns(2)
+    with col1:
+        with st.form("search_form"):
+            user_query = st.text_area("検索キーワードを入力してください", height=50)
+            submit_button = st.form_submit_button("検索")
+
+    # 検索実行
+    with col2:
+        if submit_button and user_query:
+            # Pineconeインデックスの初期化
+            index = sh.initialize_pinecone()
+
+            # クエリの実行
+            query_results = sh.perform_similarity_search(index, user_input, "ns3", top_k=5)
+            titles = sh.get_search_results_titles(query_results)
+            original_titles = sh.generate_new_titles(user_input, titles)
+
+            # クエリ結果の表示
+            # st.write("## 検索結果")
+            # if query_results.matches:
+            #     for result in query_results.matches:
+            #         # st.write(f"**ID:** {result.id}")
+            #         # st.write(f"**スコア:** {result.score:.2f}")
+            #         if 'metadata' in result:
+            #             metadata = result.metadata
+            #             st.write(f"**表紙タイトル:** {metadata.get('1枚目-表紙 (タイトル)', 'N/A')}")
+            # else:
+            #     st.write("検索結果が見つかりませんでした。")
+
+            # # 結果の出力
+            # # st.write("生成されたタイトル案:")
+            # for title in original_titles.split('\n'):
+            #     if title.strip():  # 空行を除外
+            #         st.write("- " + title)
+            st.session_state['reccomend_title'] = [f"- {title}" for title in original_titles.split('\n') if title.strip()]
+        display_titles = st.session_state.get('reccomend_title', "")
+        st.text_area("生成されたタイトル案:", "\n".join(display_titles), height=500)
