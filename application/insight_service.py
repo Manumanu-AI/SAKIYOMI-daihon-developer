617ff80aaa6707fa0bb1f018ac1bcd43464d40ae
Manumanu-AI <155441413+Manumanu-AI@users.noreply.github.com>
2024-07-07 22:18:21 +0900

    Update insight_service.py

--- application/insight_service.py
+++ application/insight_service.py
@@ -26,3 +26,19 @@ class InsightService:
 
     def get_user_ids(self) -> List[str]:
         return self.repository.get_user_ids()
+
+    def create_new_insight(self) -> Insight:
+        new_post_id = str(uuid.uuid4())
+        new_insight = Insight(
+            post_id=new_post_id,
+            created_at=datetime.now(),
+            first_view="",
+            followers_reach_count=0,
+            like_count=0,
+            new_reach_count=0,
+            plot="",
+            reach_count=0,
+            save_count=0
+        )
+        self.repository.create_insight(new_insight)
+        return new_insight
