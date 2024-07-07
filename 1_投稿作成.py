f8f5cfe5dbb9b8ea5c73bf813fe50de6ede84aef
Manumanu-AI <155441413+Manumanu-AI@users.noreply.github.com>
2024-07-07 18:13:16 +0900

    Update 1_投稿作成.py

--- 1_投稿作成.py
+++ 1_投稿作成.py
@@ -80,7 +80,7 @@ def main():
     if not st.session_state['logged_in']:
         st.markdown("# SAKIYOMI Intelligence")
         with st.container():
-            st.subheader('ログインして始める')
+            st.markdown('ログインして始める')
             email = st.text_input('Email')
             password = st.text_input('Password', type='password')
             login_button = st.button('ログイン')
