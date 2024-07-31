fd9adeb6c7bbfd2a2e853abd31e5121b11ade455
Manumanu-AI <155441413+Manumanu-AI@users.noreply.github.com>
2024-07-31 19:40:16 +0900

    Update 2_ツールの使い方.py

--- pages/2_ツールの使い方.py
+++ pages/2_ツールの使い方.py
@@ -21,42 +21,14 @@ st.markdown(custom_css, unsafe_allow_html=True)
 st.title('ツールの使い方')
 st.markdown('#')
 
-st.header('台本の生成方法')
+st.header('ツールの使い方 - 24/7/31更新')
 
-st.video('https://youtu.be/SZjbQvWrXYA?si=iI3j92OLbI5YiXfl')
+st.video('https://youtu.be/qCmEa33JKSA')
 
-st.markdown('#')
-st.subheader('操作方法')
-text1 = '''
-<p class="tighter">
-<b>1. 「生成指示」に下記の3項目を入力する</b><br>
-- テーマ<br>
-- ターゲット<br>
-- その他の指示<br>
-（<a href="https://www.notion.so/SAKIYOMI-AI-for-12-25-2924739ce4a84017a90384e01c7b00b5?pvs=21" target="_blank">参考例</a>）<br>
+st.header('新バージョンの概要')
 
+st.video('https://youtu.be/TnEDGd1SKA0')
 
-<b>2. 「参考URL」にAIに参照させたいサイトのURLを必ず1つ入力する</b><br>
-＊InstagramのURLやYouTube、画像素材サイトは使用不可<br>    
-<b>3. 「送信」を押す</b>
-</p>
-'''
-
-st.markdown(text1, unsafe_allow_html=True)
-
-st.markdown('#')
-
-
-# カスタムCSSを適用したテキストを表示
-text2 = '''
-<p class="tighter">
-	<b>注意点</b> <br>
-	生成中にPCがスリープモードに入ると、生成がストップします。<br>
-	スリープモードを1〜2分で設定してる人は、5分へ設定変更してください。
-</p>
-'''
-
-st.markdown(text2, unsafe_allow_html=True)
 
 st.markdown('##')
 
@@ -71,13 +43,27 @@ A.<br>
 <b>Q. 参考URLは1個しか登録出来ないですか？または、参考URL0個は可能？</b><br>
 A.<br>
 現時点では1個のみです。<br>
-0個だとエラーになるため、最低でも1個は必要となります。<br>
+LP、ブログ、Note等のURLを読み込むことができます。<br>
 
 <b>Q. 参考URLに動画・画像・Instagramの投稿は登録可能？</b><br>
 A.<br>
-登録不可です。<br>
-また画像素材サイトも使用出来ません。<br>
+登録不可となります。<br>
+画像素材サイト等もご使用いただけません。<br>
+</p>
+'''
+
+# カスタムCSSを適用したテキストを表示
+text2 = '''
+<p class="tighter">
+	<b>注意点</b> <br>
+	生成中にPCがスリープモードに入ると、生成がストップします。<br>
+	スリープモードを1〜2分で設定してる人は、5分へ設定変更してください。
 </p>
 '''
 
 st.markdown(text3, unsafe_allow_html=True)
+
+st.markdown(text2, unsafe_allow_html=True)
+
+
+
